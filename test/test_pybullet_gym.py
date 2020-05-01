3c5cdf5ca0815066b2eb9ecbb1cad83e3abf1e8f
Daehyung Park <daehyung@csail.mit.edu>
2020-05-01 11:47:07 -0400

    control bar for test pybullet gym

--- test/test_pybullet_gym.py
+++ test/test_pybullet_gym.py
@@ -14,10 +14,24 @@ _ = env.render(mode='human')
 print ("action is from %s to %s"%
        (env.action_space.low,env.action_space.high))
 
+env.isRender=True
 s = env.reset()
 cnt,rsum = 0,0
+
+p = env.robot._p
+x_vel = p.addUserDebugParameter('x_vel', -env.robot.jdict['x_slider'].jointMaxVelocity, env.robot.jdict['x_slider'].jointMaxVelocity, 0)
+y_vel = p.addUserDebugParameter('y_vel', -env.robot.jdict['y_slider'].jointMaxVelocity, env.robot.jdict['y_slider'].jointMaxVelocity, 0)
+th_vel = p.addUserDebugParameter('th_vel', -env.robot.jdict['z_axis_joint'].jointMaxVelocity, env.robot.jdict['z_axis_joint'].jointMaxVelocity, 0)
+#from IPython import embed; embed(); sys.exit()
+
 while 1:
     cnt += 1 # increase counter
+
+    # manual
+    ## a = [p.readUserDebugParameter(x_vel),\
+    ##      p.readUserDebugParameter(y_vel),\
+    ##      p.readUserDebugParameter(th_vel)]
+    
     a = env.action_space.sample()
     obs, r, done, _ = env.step(action=a)
     ## print (obs, r, done)
@@ -27,7 +41,7 @@ while 1:
     if done:
         env.reset()
     ##     break
-    time.sleep(0.04)
+    time.sleep(0.01)
 p.disconnect()
 env.close()
 print ("Done. ravg:[%.3f]"%(rsum/cnt))
