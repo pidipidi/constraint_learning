4fe16fd352e9901231f435758afe496cc9caebbf
Daehyung Park <daehyung@csail.mit.edu>
2020-05-07 00:45:37 -0400

    added a peg-in-hole with a simulated gripper

--- test/test_pybullet_gym.py
+++ test/test_pybullet_gym.py
@@ -8,40 +8,50 @@ import pybulletgym  # register PyBullet enviroments with open ai gym
 
 #p.connect(p.DIRECT)
 #env = gym.make("ReacherPyBulletEnv-v0")
-env = gym.make("CuboidPeginhole2DPyBulletEnv-v0")
 #env = gym.make("InvertedPendulumPyBulletEnv-v0")
+#env = gym.make("CuboidPegInHole2DPyBulletEnv-v0")
+#env = gym.make("CuboidPegInShallowHole2DPyBulletEnv-v0")
+env = gym.make("GripperPegInHole2DPyBulletEnv-v0")
 _ = env.render(mode='human')
-print ("action is from %s to %s"%
-       (env.action_space.low,env.action_space.high))
+#from IPython import embed; embed(); sys.exit()
 
 env.isRender=True
 s = env.reset()
 cnt,rsum = 0,0
+env.action_space = env.robot.action_space
+env.observation_space = env.robot.observation_space
+#from IPython import embed; embed(); sys.exit()
 
 p = env.robot._p
 x_vel = p.addUserDebugParameter('x_vel', -env.robot.jdict['x_slider'].jointMaxVelocity, env.robot.jdict['x_slider'].jointMaxVelocity, 0)
 y_vel = p.addUserDebugParameter('y_vel', -env.robot.jdict['y_slider'].jointMaxVelocity, env.robot.jdict['y_slider'].jointMaxVelocity, 0)
 th_vel = p.addUserDebugParameter('th_vel', -env.robot.jdict['z_axis_joint'].jointMaxVelocity, env.robot.jdict['z_axis_joint'].jointMaxVelocity, 0)
+finger_pos = p.addUserDebugParameter('gripper_pos', 0.005, 0.13, 0.005)
 #from IPython import embed; embed(); sys.exit()
 
 while 1:
     cnt += 1 # increase counter
 
     # manual
-    ## a = [p.readUserDebugParameter(x_vel),\
-    ##      p.readUserDebugParameter(y_vel),\
-    ##      p.readUserDebugParameter(th_vel)]
+    a = [p.readUserDebugParameter(x_vel),\
+         p.readUserDebugParameter(y_vel),\
+         p.readUserDebugParameter(th_vel),\
+         p.readUserDebugParameter(finger_pos) ]
     
     a = env.action_space.sample()
+    #a[-1] = 0.0
+    #a = [0,0,0,0]
     obs, r, done, _ = env.step(action=a)
-    ## print (obs, r, done)
+    ## print (a, obs[0],obs[2],obs[4],obs[6])
+    
     rsum += r
     still_open = env.render(mode='human')
     ## print ("[%d] r:[%.2f] done:[%d]"%(cnt,r,done))
     if done:
         env.reset()
     ##     break
-    time.sleep(0.01)
+    time.sleep(1./240.)
+    #time.sleep(1./100.)
 p.disconnect()
 env.close()
 print ("Done. ravg:[%.3f]"%(rsum/cnt))
